7ad3968a318393a608c57476c9f884d1bbc8d600
normyee <lunoqro@gmail.com>
2025-08-13 19:58:06 -0300

    feat: add decodeHttpRequest private method

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,62 @@
 import net from "net";
+import { BadRequestError } from "./errors";
 
 const PORT = 8181;
 
-const server = net.createServer((socket) => {
-    console.log(`TCP connection established from ${socket.remoteAddress}:${socket.remotePort}`);
+export class FayuServer {
+  private _app: net.Server;
 
-    socket.on("data", (data) => {
-        console.log(`Received data: ${data.toString()}`);
+  private _decodeHttpRequest(clientRequest: Buffer<ArrayBufferLike>) {
+    try {
+      const requestLine = clientRequest.toString().split("\r\n")[0];
+      const [method, path, version] = requestLine.split(" ");
 
+      if (!method || !path || !version)
+        throw new BadRequestError("Missing method, path or version");
+
+      return {
+        method,
+        path,
+        version,
+      };
+    } catch (error) {
+      console.error(error);
+      //IDK GONNA FIGURE IT OUT
+    }
+  }
+
+  constructor() {
+    this._app = net.createServer((socket) => {
+      console.log(
+        `TCP connection established from ${socket.remoteAddress}:${socket.remotePort}`
+      );
+
+      socket.on("data", (data) => {
+        console.log(
+          `Received data: 
+            ${data.toString()}
+            `
+        );
+
+        const decodedHttpRequest = this._decodeHttpRequest(data);
+        console.log(decodedHttpRequest);
+      });
+
+      socket.on("end", () => {
+        console.log(
+          `Connection from ${socket.remoteAddress}:${socket.remotePort} ended`
+        );
+      });
     });
+  }
 
-    socket.on("end", () => {
-        console.log(`Connection from ${socket.remoteAddress}:${socket.remotePort} ended`);
-    })
-});
+  start() {
+    this._app.listen(PORT, () => {
+      console.log(`TCP server is listening on port 8080`);
+    });
+  }
+}
 
-server.listen(PORT, () => {
-    console.log(`TCP server is listening on port 8080`);
-});
+const server = new FayuServer();
+
+server.start();
